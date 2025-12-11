670984ec38d31e6ed1680c2535542b435ba43dff
Detailmovie0 <moviehub4uofficial0@gmail.com>
2025-12-11 12:27:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1238,10 +1238,10 @@ async def auto_filter(client, msg, spoll=False):
             settings = await get_settings(message.chat.id)
             if not files:
                 if settings["spell_check"]:
-                    ai_sts = await m.edit('🤖 ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ, ᴀɪ ɪꜱ ᴄʜᴇᴄᴋɪɴɢ ʏᴏᴜʀ ꜱᴘᴇʟʟɪɴɢ...')
+                    ai_sts = await m.edit('<b>ᴡᴀɪᴛ, ᴀɪ ᴄᴏʀʀᴇᴄᴛɪɴɢ ʏᴏᴜʀ ꜱᴘᴇʟʟɪɴɢ 🌿</b>')
                     is_misspelled = await ai_spell_check(chat_id = message.chat.id,wrong_name=search)
                     if is_misspelled:
-                        await ai_sts.edit(f'<b>✅Aɪ Sᴜɢɢᴇsᴛᴇᴅ ᴍᴇ<code> {is_misspelled}</code> \nSᴏ Iᴍ Sᴇᴀʀᴄʜɪɴɢ ғᴏʀ <code>{is_misspelled}</code></b>')
+                        await ai_sts.edit(f'<b>ᴀɪ ꜱᴜɢɢᴇꜱᴛᴇᴅ 🌿<code> {is_misspelled}</code> \nꜱᴏ ɪ ᴀᴍ ꜱᴇᴀʀᴄʜɪɴɢ ᴛʜɪꜱ 🌿<code>{is_misspelled}</code></b>')
                         await asyncio.sleep(2)
                         message.text = is_misspelled
                         await ai_sts.delete()
